134793fa3465018e1832c41596da081227bc116c
Your Name <you@example.com>
2020-01-13 17:33:28 -0800

    further study for dictionaries

--- ratings.py
+++ ratings.py
@@ -2,7 +2,7 @@
 #define funcion that takes in text file name as argument
 #open and store text file data into a file handle object
 #use a for loop to create a list of words (use rstrip and split methods)
-
+# 
 
 
 # put your code here
@@ -14,5 +14,18 @@ def sort_restaurant_ratings(filename):
     dictionary_of_ratings = {}
 
     for line in text_file:
-        line = line.rstrip() # why is this different than just line.rstrip() ?
-        words = line.split(' ')
+        line = line.rstrip()
+        words = line.split(':')
+        # print(words)
+
+        dictionary_of_ratings[words[0]] = words[1]
+
+        sorted_list_of_keys = sorted(dictionary_of_ratings)
+
+   
+    for key in sorted_list_of_keys:
+        print(f'{key} is rated at {dictionary_of_ratings[key]}')
+        # print("{} is rated at {}".format(key, dictionary_of_ratings[key]))
+
+
+sort_restaurant_ratings('scores.txt')
